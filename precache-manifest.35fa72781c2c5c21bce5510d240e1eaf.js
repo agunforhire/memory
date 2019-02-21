1e0a23f032aa7777db60f41f702a393ee842eb0e
James Wilcox <architeuthisdux@protonmail.com>
2019-02-20 18:39:22 -0800

    Updates

---
+++ precache-manifest.35fa72781c2c5c21bce5510d240e1eaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bba3517bfce1346f5e2a",
+    "url": "/memory/static/js/runtime~main.bba3517b.js"
+  },
+  {
+    "revision": "c54ab9a939b0353f483a",
+    "url": "/memory/static/js/main.c54ab9a9.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/memory/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "c54ab9a939b0353f483a",
+    "url": "/memory/static/css/main.aad68285.chunk.css"
+  },
+  {
+    "revision": "453a78d121592fbcd7e62ee309547284",
+    "url": "/memory/index.html"
+  }
+];
